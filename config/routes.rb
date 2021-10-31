2a2b1c3a12dfe59b23bd76dce029f8b3768441a8
TanBinh1601 <tanbinhjk1601@gmail.com>
2021-10-31 23:24:02 +0700

    chapter_3_4_5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    resources :home
+    root 'static_pages#home'
+  end
 end
