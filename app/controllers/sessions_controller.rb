4c27f609e98d7329a56bfc2c511f1e8512cfecf0
TanBinh1601 <tanbinhjk1601@gmail.com>
2021-10-31 23:24:02 +0700

    chapter_9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,16 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
       log_in user
+      remember user
       redirect_to user
     else
-      flash.now[:danger] = t "invalid_email_password_combination"
+      flash.now[:danger] = t "user.login.check_fails"
       render :new
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
